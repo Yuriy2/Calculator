40eac315bdd2f92d62d50741d4f9905a187c8afc
Yura Chaban <chabann22@gmail.com>
2020-05-31 13:57:35 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
